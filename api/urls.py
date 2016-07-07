714305f9cb5b438c13d0bd01c00056fb4c6b7e1f
rjesteban <ajcp.esteban@gmail.com>
2016-07-07 15:12:55 +0800

    Refactor Url Code

---
+++ api/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import include, url
+from rest_framework import routers
+from api import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'clients', views.UserProfileViewSet)
+router.register(r'groups', views.GroupViewSet)
+router.register(r'shops', views.LaundryShopViewSet)
+router.register(r'transactions', views.TransactionViewSet)
+router.register(r'orders', views.OrderViewSet)
+router.register(r'prices', views.PriceViewSet)
+router.register(r'services', views.ServiceViewSet)
+
+urlpatterns = [
+    url(r'^', include(router.urls))
+]
