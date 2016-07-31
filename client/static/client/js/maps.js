0e1793921db5b81f16a5794edbc8c9272a8d3f21
rjesteban <ajcp.esteban@gmail.com>
2016-07-31 18:47:11 +0800

    Fix Info Window

--- client/static/client/js/maps.js
+++ client/static/client/js/maps.js
@@ -23,52 +23,69 @@ function displayMap() {
   });
 }
 
+
+
+function populateInfoWindow(name, location, id){
+  var infoContent =
+  "<div id='content'>"+
+    "<h5 id='firstHeading' class = 'firstHeading'>"+ name+"</h5>"+
+    "<div id='bodyContent'>"+
+      "<p>" + location + "</p>"+
+      "<a href='/order/"+ id + "' class='button'>Order</a>"+
+    "</div>"+
+  "</div>";
+
+  var infoWindow = new google.maps.InfoWindow({
+    content: infoContent,
+    maxWidth : 200
+  });
+  return infoWindow;
+}
+
 function markLaundryLocation(geocoder, resultsMap) {
+  var laundryMarker = {
+    url: laundryIcon,
+    size: new google.maps.Size(40, 50),
+    origin: new google.maps.Point(0, 0),
+    anchor: new google.maps.Point(0, 32)
+  };
+
   for(var i = 0 ; i<parsedShopList.length ; i++){
-    var infoContent =
-    "<div id='content'>"+
-      "<h5 id='firstHeading' class = 'firstHeading'>"+ parsedShopList[i].name+"</h5>"+
-      "<div id='bodyContent'>"+
-        "<p>" + parsedShopList[i].location + "</p>"+
-      "</div>"+
-    "</div>";
-
-    var infoWindow = new google.maps.InfoWindow({
-      content: infoContent,
-      maxWidth : 200
-    })
-
-    geocoder.geocode({'address':parsedShopList[i].location}, function(results, status) {
-      if (status === google.maps.GeocoderStatus.OK) {
-        console.log(laundryIcon);
-        var laundryMarker = {
-          url: laundryIcon,
-          // This marker is 20 pixels wide by 32 pixels high.
-          size: new google.maps.Size(40, 50),
-          // The origin for this image is (0, 0).
-          origin: new google.maps.Point(0, 0),
-          // The anchor for this image is the base of the flagpole at (0, 32).
-          anchor: new google.maps.Point(0, 32)
-        };
+    var match = $.when(geocodeThis(geocoder, parsedShopList[i].location), parsedShopList[i]);
+    match.done(function (position, shop) {
+        var infoWindow = populateInfoWindow(shop.name, shop.location, shop.id);
         var marker = new google.maps.Marker({
           map: resultsMap,
-          position: results[0].geometry.location,
+          position: position,
           icon: laundryMarker
         });
         marker.addListener('click',function(){
-          infoWindow.open(resultsMap,marker);
+          infoWindow.open(resultsMap, marker);
         });
-      } else {
-        alert('Geocode was not successful for the following reason: ' + status);
-      }
-    });
+
+      });
   }
+}
+
+
+function geocodeThis(geocoder, address){
+  var deferred = $.Deferred();
+  geocoder.geocode({'address':address}, function(results, status) {
+    if (status === google.maps.GeocoderStatus.OK) {
+        var position = results[0].geometry.location;
+        deferred.resolve(position);
+    } else {
+      var message = 'Geocode was not successful for the following reason: ' + status;
+      alert(message);
+      deferred.reject(message);
+    }
+  });
+  return deferred.promise();
 }
 
 
 function marklocation(geocoder, map, coordinates){
     geocoder.geocode({'location': coordinates}, function(results, status) {
-        console.log('twwwooo');
         if (status === google.maps.GeocoderStatus.OK) {
             if (results[0]) {
               var marker = new google.maps.Marker({
